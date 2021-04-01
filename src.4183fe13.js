41066b4c49a6023151ab060818ad0e4b445b0ffc
MarianMoskal <77517344+MarianMoskal@users.noreply.github.com>
2021-04-01 18:02:36 +0000

    Deploying to gh-pages from @ MarianMoskal/team-20-project@edcb984122feaafd4770aa81d3cc7cb33b7095c2 🚀

--- src.4183fe13.js
+++ src.4183fe13.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-20-project/src.27cfd8f0.js.map
+//# sourceMappingURL=/team-20-project/src.4183fe13.js.map
